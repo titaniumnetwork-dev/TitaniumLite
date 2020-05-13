a13ce6d8d77d2b8e1601c9d8b9edaf29836194f9
LQ16 <59111554+LQ16@users.noreply.github.com>
2020-05-12 21:33:00 -0400

    not getting why this isn't working, so I'm pulling old code. It worked locally, but not on shitoku

--- templates/main.php
+++ templates/main.php
@@ -1,14 +1,9 @@
-<style>img[alt*="www.000webhost.com"] {
-    display: none;}
-code made by Titanumnetwork,
-img[alt="www.000webhost.com"]{display:none;}
-</style>
-<html>
-<!DOCTYPE html>
 <html>
 <head>
-<body style="background-color:black;">
-<title>TitaniumNetwork Proxy</title>
+<body>
+<title>My Drive - Google Drive</title>
+    <link rel="icon"
+          href="https://ssl.gstatic.com/docs/doclist/images/infinite_arrow_favicon_4.ico">
 
 <meta name="generator" content="php-proxy.com">
 <meta name="version" content="<?=$version;?>">
@@ -22,7 +17,7 @@ html body {
 #container {
 	width:500px;
 	margin:0 auto;
-	margin-top:150px;
+	margin-top:50px;
 }
 
 #error {
@@ -32,9 +27,9 @@ html body {
 
 #frm {
 	padding:10px 15px;
-	background-color:#1f1f14;
 
-	border:1px solid #6600ff;
+
+	border:1px solid #ffffff;
 
 	-webkit-border-radius: 8px;
 	-moz-border-radius: 8px;
@@ -49,17 +44,74 @@ html body {
 }
 </style>
 
+<style>
+
+a.button1{
+ display:inline-block;
+ padding:1.5em 3.0em;
+ border:0.1em solid #FFFFFF;
+ margin:0 0.3em 0.3em 0;
+ border-radius:0.12em;
+ box-sizing: border-box;
+ text-decoration:none;
+ font-family:'Roboto',sans-serif;
+ font-weight:300;
+ color:#FFFFFF;
+ text-align:center;
+ transition: all 0.2s;
+ margin-top: 15px;
+}
+a.button1:hover{
+ color:#000000;
+ background-color:#FFFFFF;
+}
+@media all and (max-width:30em){
+ a.button1{
+  display:block;
+  margin:0.4em auto;
+ }
+}
+</style>
+
+<style>
+.js-loading *,
+.js-loading *:before,
+.js-loading *:after {
+  animation-play-state: paused !important;
+}
+</style>
+
+
+<body style="background-color: black;">
+
+
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+
+<script>
+document.body.classList.add('js-loading');
+
+window.addEventListener("load", showPage);
+
+function showPage() {
+  document.body.classList.remove('js-loading');
+}
+</script>
+
+
+
 </head>
 
 <body>
 
 
-<div id="container">
+
+
+
+<div id="container" style="text-align:center;" class="w3-animate-zoom" >
 
 	<div style="text-align:center;">
 		<h1 style="color:purple;">Titanium Network Proxy</h1>
-    <h1 style="color:purple;">Fallback Page</h1>
-                <h1 style="color:green;">Local PHP-Proxy</h1>
+                <h1 style="color:green;">Local Proxy</h1>
 		<h3 style="color:blue;">Using the titaniumnetwork proxy will allow you to anonymously visit any site you wish. Due to its anonymous nature, you can even unblock websites that are blocked by school and work firewalls!</h3>
 	</div>
 
@@ -75,7 +127,7 @@ html body {
 
 	<!-- I wouldn't touch this part -->
 
-		<form action="index.php" method="post" style="margin-bottom:0;">
+		<form action="index.php" method="post" style="margin-top:0;">
 			<input name="url" type="text" style="width:400px;" autocomplete="off" placeholder="http://" />
 			<input type="submit" value="Go" />
 		</form>
@@ -89,19 +141,18 @@ html body {
 	</div>
 
 </div>
-	<div style="text-align:center;">
-		<h2 style="color:blue;">Websites that can be unblocked include the following : </h2>
-		<h3 style="color:green;">Youtube</h3>
-		<h3 style="color:green;">Reddit</h3>
-		<h3 style="color:green;">Twitter</h3>
-		<h3 style="color:green;">Dailymotion</h3>
-		<h3 style="color:green;">Liveleaks</h3>
-		<h3 style="color:green;">And many more!</h3>
-	    </div
-<div id="footer">
-	Powered by <a href="#" target="_blank">TitaniumNetwork</a>
-</div>
+	<div style="text-align:center; " class="w3-animate-bottom">
+		<h4 style="color:blue;">Note: this proxy is so broken and screwed up that most things will not work, including basic stuff. Be warned.</h2>
+	    </div>
 
 
 </body>
 </html>
+
+<script src="js/crate.js" async defer>
+  new Crate({
+    server: '419123358698045453',
+    channel: '478975047521009667',
+    shard: 'https://cl3.widgetbot.io'
+  })
+</script>
--- templates/url_form.php
+++ templates/url_form.php
@@ -1,4 +1,7 @@
+<html>
 
+<link rel="icon" href="https://ssl.gstatic.com/docs/doclist/images/infinite_arrow_favicon_5.ico">
+<title>My Drive - Google Drive</title>
 <style type="text/css">
 
 html body {
@@ -8,23 +11,23 @@ html body {
 #top_form {
 	position: fixed;
 	top:0;
-	text-align:center;
+	left:0;
 	width: 100%;
-
+	
 	margin:0;
-
+	
 	z-index: 2100000000;
-	-moz-user-select: none;
-	-khtml-user-select: none;
-	-webkit-user-select: none;
-	-o-user-select: none;
-
-	border-bottom:1px solid #151515;
-
-    background:gray;
-
-	height:37px;
-	line-height:35px;
+	-moz-user-select: none; 
+	-khtml-user-select: none; 
+	-webkit-user-select: none; 
+	-o-user-select: none; 
+	
+	border-bottom:1px solid #CE00C0;
+	
+    background:#4D0243;
+	
+	height:45px;
+	line-height:45px;
 }
 
 #top_form input[name=url] {
@@ -38,6 +41,8 @@ html body {
 
 </style>
 
+<link rel="icon" href="https://ssl.gstatic.com/docs/doclist/images/infinite_arrow_favicon_5.ico">
+
 <script>
 var url_text_selected = false;
 
@@ -46,7 +51,7 @@ function smart_select(ele){
 	ele.onblur = function(){
 		url_text_selected = false;
 	};
-
+	
 	ele.onclick = function(){
 		if(url_text_selected == false){
 			this.focus();
@@ -60,18 +65,21 @@ function smart_select(ele){
 <div id="top_form">
 
 	<div style="width:800px; margin:0 auto;">
-
+	
 		<form method="post" action="index.php" target="_top" style="margin:0; padding:0;">
 			<input type="button" value="Home" onclick="window.location.href='index.php'">
 			<input type="text" name="url" value="<?php echo $url; ?>" autocomplete="off">
 			<input type="hidden" name="form" value="1">
 			<input type="submit" value="Go">
 		</form>
-
+		
 	</div>
-
+	
 </div>
 
 <script type="text/javascript">
 	smart_select(document.getElementsByName("url")[0]);
 </script>
+
+	
+<link rel="icon" href="https://ssl.gstatic.com/docs/doclist/images/infinite_arrow_favicon_5.ico">
